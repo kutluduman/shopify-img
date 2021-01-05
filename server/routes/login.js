d35c346320798488e0dcc04b9496633908719edb
kutluduman <kutluduman@gmail.com>
2021-01-05 03:44:32 -0500

    Created the register route and also formatted login and register routes

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
 
 module.exports = (db) => {
-
   router.post("/", (req, res) => {
     const email = req.body.user.email;
 
-    return db.query(`SELECT id, first_name, last_name, email, phone_number, password
+    return db
+      .query(
+        `SELECT id, first_name, last_name, email, phone_number, password
     FROM users
-    WHERE email = $1;`, [email])
+    WHERE email = $1;`,
+        [email]
+      )
       .then((data) => {
         if (bcrypt.compareSync(req.body.user.password, data.rows[0].password)) {
           const user = data.rows[0];
           res.json({
-            user
+            user,
           });
         } else {
           res.status(401).send("Incorrect Email or Password");
@@ -23,11 +25,10 @@ module.exports = (db) => {
       })
       .catch((err) => {
         res.status(500).json({
-          error: err.message
+          error: err.message,
         });
       });
   });
 
   return router;
-
 };
---
+++ server/routes/register.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+module.exports = (db) => {
+  router.post("/", (req, res) => {
+    const user = req.body;
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const email = req.body.email;
+    const password = bcrypt.hashSync(user.password, 10);
+
+    return db
+      .query(
+        `
+    SELECT *
+    FROM users
+    WHERE email = $1;
+    `,
+        [email]
+      )
+      .then((data) => {
+        if (data.rows.length > 0) {
+          res.status(401).send("Email already exists");
+        } else {
+          db.query(
+            `
+        INSERT INTO users (first_name, last_name, email, password)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *;
+        `,
+            [first_name, last_name, email, password]
+          )
+            .then((data) => {
+              const user = data.rows;
+              res.json({
+                users,
+              });
+            })
+            .catch((err) => {
+              res.status(500).json({
+                error: err.message,
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err.message,
+        });
+      });
+  });
+
+  return router;
+};
