48ffff9a45fa8e96749fa0d3d77adcbda67d5563
kutluduman <kutluduman@gmail.com>
2021-01-04 23:10:28 -0500

    Created database table for users and seeded info

---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, email, password, phone_number) VALUES ('James', 'Smith', 'jamessmith@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (first_name, last_name, email, password, phone_number) VALUES ('Michael', 'Paul', 'michaelpaul@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (first_name, last_name, email, password, phone_number) VALUES ('Robert', 'Smith', 'robertsmith@email.com', 'hel$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.lo');
+INSERT INTO users (first_name, last_name, email, password, phone_number) VALUES ('Maria', 'Garcia', 'mariagarcia@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
