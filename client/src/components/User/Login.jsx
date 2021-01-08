139b7c35ccfc3439d7a86842ef3792e0bd0b7df0
kutluduman <kutluduman@gmail.com>
2021-01-08 18:12:18 -0500

    Fade Title for the form is added

--- client/src/components/User/Login.jsx
+++ client/src/components/User/Login.jsx
@@ -1,28 +1,42 @@
+import { useState } from "react";
 import Navbar from "../Home/Navbar";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
-const LoginContainer = styled.div`
-position:fixed;
-padding:0;
-margin:0;
+const PageContainer = styled.div`
+  position: fixed;
+  padding: 0;
+  margin: 0;
 
-top:20;
-left:0;
+  top: 20;
+  left: 0;
 
-width: 100%;
-height: 100%;
-background-color: #CCD7D1;
+  width: 100%;
+  height: 100%;
+  background-color: #ccd7d1;
 
-
+  .form-title {
+    color: black;
+    display: flex;
+    justify-content: center;
+    margin-top: 2em;
+  }
 `;
 
 const Login = () => {
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+
   return (
     <div>
       <Navbar />
-      <LoginContainer>
-        <p>Hello From Login</p>
-      </LoginContainer>
+      <PageContainer>
+        <Fade top>
+          <h2 className="form-title">Login</h2>
+        </Fade>
+      </PageContainer>
     </div>
   );
 };
