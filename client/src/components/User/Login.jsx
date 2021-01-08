802949b9c17d6647b62066d02454f9ae0b9673f9
kutluduman <kutluduman@gmail.com>
2021-01-08 16:56:52 -0500

    Login background color is configured

--- client/src/components/User/Login.jsx
+++ client/src/components/User/Login.jsx
@@ -1,10 +1,28 @@
 import Navbar from "../Home/Navbar";
+import styled from "styled-components";
+
+const LoginContainer = styled.div`
+position:fixed;
+padding:0;
+margin:0;
+
+top:20;
+left:0;
+
+width: 100%;
+height: 100%;
+background-color: #CCD7D1;
+
+
+`;
 
 const Login = () => {
   return (
     <div>
       <Navbar />
-      <p>Hello From Login</p>
+      <LoginContainer>
+        <p>Hello From Login</p>
+      </LoginContainer>
     </div>
   );
 };
