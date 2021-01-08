33d6b89a12cbb9540bc410dcc18742ebe522b718
kutluduman <kutluduman@gmail.com>
2021-01-08 18:35:52 -0500

    Fade is added for the register page as well

--- client/src/components/User/Register.jsx
+++ client/src/components/User/Register.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import Navbar from "../Home/Navbar";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
+
+const PageContainer = styled.div`
+  position: fixed;
+  padding: 0;
+  margin: 0;
+
+  top: 20;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+  background-color: #ccd7d1;
+
+  .form-title {
+    color: black;
+    display: flex;
+    justify-content: center;
+    margin-top: 2em;
+  }
+`;
 
 const Register = () => {
   return (
     <div>
       <Navbar />
-      <p>Hello From Register</p>
+      <PageContainer>
+        <Fade top>
+          <h2 className="form-title">Register</h2>
+        </Fade>
+      </PageContainer>
     </div>
   );
 };
