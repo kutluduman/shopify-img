a135727afffd9db97551b7a4c248260f678d4c79
kutluduman <kutluduman@gmail.com>
2021-01-08 23:24:36 -0500

    Register page css is ready. Logic will be implemented

--- client/src/components/User/Register.jsx
+++ client/src/components/User/Register.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Navbar from "../Home/Navbar";
 import styled from "styled-components";
 import Fade from "react-reveal/Fade";
+import { TextField } from "@material-ui/core";
+import { Button } from '@material-ui/core';
 
 const PageContainer = styled.div`
   position: fixed;
@@ -21,9 +23,28 @@ const PageContainer = styled.div`
     justify-content: center;
     margin-top: 2em;
   }
+
+  form.form {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin-top: 2em;
+    margin-left: 50em;
+    width: 25em;
+  }
+  form.form .MuiFormControl-root {
+    margin-bottom: 1em;
+  }
+  
 `;
 
 const Register = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return (
     <div>
       <Navbar />
@@ -31,6 +52,55 @@ const Register = () => {
         <Fade top>
           <h2 className="form-title">Register</h2>
         </Fade>
+        <form className="form" onSubmit={(e) => e.preventDefault()}>
+          <TextField
+            required={true}
+            // onChange={(event) =>
+            //   setStateLogin({ ...stateLogin, username: event.target.value })
+            // }
+            label="First Name"
+            // value={stateLogin.username}
+            variant="filled"
+            size="medium"
+            fullfullWidth={true}
+          />
+          <TextField
+            // onChange={(event) =>
+            //   setStateLogin({ ...stateLogin, password: event.target.value })
+            // }
+            label="Last Name"
+            // value={stateLogin.password}
+            variant="filled"
+            size="medium"
+            fullfullWidth={true}
+          />
+          <TextField
+            required={true}
+            // onChange={(event) =>
+            //   setStateLogin({ ...stateLogin, username: event.target.value })
+            // }
+            label="Email"
+            // value={stateLogin.username}
+            variant="filled"
+            size="medium"
+            fullfullWidth={true}
+          />
+          <TextField
+            required={true}
+            type="password"
+            // onChange={(event) =>
+            //   setStateLogin({ ...stateLogin, username: event.target.value })
+            // }
+            label="Password"
+            // value={stateLogin.username}
+            variant="filled"
+            size="medium"
+            fullfullWidth={true}
+          />
+          <Button fullWidth variant="contained" color="primary" type="submit">
+            REGISTER
+          </Button>
+        </form>
       </PageContainer>
     </div>
   );
