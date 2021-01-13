05d38cce94c797adc9e92fee714f284ab3e75504
kutluduman <kutluduman@gmail.com>
2021-01-13 13:33:19 -0500

    Created Gallery component

---
+++ client/src/components/Home/Gallery.jsx
@@ -0,0 +1,8 @@
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+const Gallery = ({ images }) => {
+  return <ImageGallery items={images} />;
+};
+
+export default Gallery;
