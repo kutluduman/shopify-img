169b48045e48e62ceaabba305d1c65f3e7c475ce
kutluduman <kutluduman@gmail.com>
2021-01-13 13:28:14 -0500

    Created the upload component

---
+++ client/src/components/Home/Uploads.js
@@ -0,0 +1,35 @@
+import Dropzone from "react-dropzone-uploader";
+import "react-dropzone-uploader/dist/styles.css";
+
+const Uploads = ({ fetchUploads }) => {
+  const getUploadParams = ({ file }) => {
+    const body = new FormData();
+    body.append("image", file);
+    return {
+      url: "http://localhost:8000/uploads",
+      body,
+    };
+  };
+
+  const handleSubmit = (files, allFiles) => {
+    allFiles.forEach((f) => f.remove());
+    fetchUploads();
+  };
+
+  return (
+    <div>
+      <Dropzone
+        getUploadParams={getUploadParams}
+        onSubmit={handleSubmit}
+        accept="image/*"
+        maxFiles={1}
+        multiple={false}
+        styles={{
+          dropzone: { minHeight: 200, maxHeight: 250 },
+        }}
+      />
+    </div>
+  );
+};
+
+export default Uploads;
